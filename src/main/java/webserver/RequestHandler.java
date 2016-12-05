a215415fc7f9c824592ececfc81a5be6791b63a7
Td Kim <yenafirst91@gmail.com>
2016-12-06 00:29:59 +0900

    added response302Header

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -9,6 +9,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.util.HashMap;
 import java.util.Map;
 
 import org.slf4j.Logger;
@@ -16,6 +17,7 @@ import org.slf4j.LoggerFactory;
 
 import model.User;
 import util.HttpRequestUtils;
+import util.IOUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -38,27 +40,54 @@ public class RequestHandler extends Thread {
         	}
 
         	String url = HttpRequestUtils.getUrl(line);
-        	if (url.startsWith("/create")) {
-        		int index = url.indexOf("?");
+        	Map<String, String> headers = new HashMap<String, String>();
+        	
+        	while(!"".equals(line)) {
+	    		log.debug("header : {}", line);
+	    		line = br.readLine();
+	    		String[] headerTokens = line.split(": ");
+	    		if (headerTokens.length == 2) {
+	    			headers.put(headerTokens[0], headerTokens[1]);
+	    		}
+	    	}
+        	
+        	log.debug("Content-Length : {}", headers.get("Content-Length"));
+        	
+        	if (url.startsWith("/user/create")) {
+        		
+        		String requestBody = IOUtils.readData(br, Integer.parseInt(headers.get("Content-Length")));
+        		log.debug("Request Body : {}", requestBody);
+        		
+        		
+//        		int index = url.indexOf("?");
 //        		String requestPath = url.substring(0, index);
-        		String queryString = url.substring(index + 1);
-        		Map<String, String> params = HttpRequestUtils.parseQueryString(queryString);
+//        		String queryString = url.substring(index + 1);
+        		Map<String, String> params = HttpRequestUtils.parseQueryString(requestBody);
         		User user = new User(params.get("userId"), params.get("password"), params.get("name"), params.get("email"));
         		
         		log.debug("User : {}", user);
         		
         		url = "/index.html";
+        		
+        		DataOutputStream dos = new DataOutputStream(out);
+        		response302Header(dos);
+        	} else {
+        		DataOutputStream dos = new DataOutputStream(out);
+        		byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
+        		response200Header(dos, body.length);
+        		responseBody(dos, body);
         	}
         	
-//        	while(!"".equals(line)) {
-//        		log.debug("header : {}", line);
-//        		line = br.readLine();
-//        	}
-        	
-            DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
-            response200Header(dos, body.length);
-            responseBody(dos, body);
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+    
+    private void response302Header(DataOutputStream dos) {
+        try {
+            dos.writeBytes("HTTP/1.1 302 Found \r\n");
+            dos.writeBytes("Location: /index.html\r\n");
+            dos.writeBytes("\r\n");
         } catch (IOException e) {
             log.error(e.getMessage());
         }
